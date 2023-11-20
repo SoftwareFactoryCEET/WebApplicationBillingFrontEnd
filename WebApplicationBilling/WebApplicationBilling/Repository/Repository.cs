50a210fbfe48cd437ad6ec1f0dd7eedba53bbb33
Vladimir Cortes A <vccortesa@eafit.edu.co>
2023-11-20 17:16:43 -0500

    feat(fronend): Add Program Class, Interfaces and DTOs

---
+++ WebApplicationBilling/WebApplicationBilling/Repository/Repository.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using WebApplicationBilling.Repository.Interfaces;
+
+namespace WebApplicationBilling.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        //Inyección de dependencias e inversión de control
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public Repository(IHttpClientFactory httpClientFactory)
+        {
+            this._httpClientFactory = httpClientFactory;
+        }
+        public Task<bool> DeleteAsync(string url, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable> GetAllAsync(string url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var client = _httpClientFactory.CreateClient();
+
+            HttpResponseMessage responseMessage = client.SendAsync(request).Result;
+
+            //Validar la respuesta
+            if (responseMessage.StatusCode  == System.Net.HttpStatusCode.OK)
+            {
+                var jsonString = await responseMessage.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<IEnumerable<T>>(jsonString);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Task<T> GetByIdAsync(string url, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PostAsync(string url, T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(string url, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
